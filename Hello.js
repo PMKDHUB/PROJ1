09466f73c19d2a11b6781ef4ca5672c92703468b
PMKDHUB <pmaheshkumardas@gmail.com>
2026-04-13 08:54:14 +0000

    comitted

--- Hello.js
+++ Hello.js
@@ -1,3 +1,11 @@
 function clickme(){
     alert("button clicked")
 }
+
+function showH1(){
+    document.getElementById("mainHeading").classList.remove("hidden");
+}
+
+function hideH1(){
+    document.getElementById("mainHeading").classList.add("hidden");
+}
